756d137a96d4f2a749a9122a622872b532e1bd2c
Max Brumberg <maks.brumberg@mail.ru>
2023-12-14 17:51:16 +0300

    dz

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] array = { 41.14, 82.71, 4.56, 21.18, 15.97, 88.32, 35.27, 49.32, 7.12, 12.52 };
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+double sum = max - min;
+Console.WriteLine($"максимальный элемент: {max}");
+Console.WriteLine($"минимальный элемент: {min}");
+Console.WriteLine($"разница между максимальным и минимальным элементами: {sum}");
